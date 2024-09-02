2b48bc372255a93a4426994e155b35802561ba73
Enzo <Enzocalasanscardosocorreacosta@gmail.com>
2024-09-02 15:44:46 -0300

    Final Commit

---
+++ src/componentes/Rodapé/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Rodape = () => {
+  return (
+    <footer className='rodape'>
+        <div>
+            <img src='/imagens/fb.png' alt=''/>
+            <img src='/imagens/tw.png' alt=''/>
+            <img src='/imagens/ig.png' alt=''/>
+        </div>
+        <div>
+            <img src='' alt=''/>
+        </div>
+        <div>
+            <img src='' alt=''/>
+        </div>
+        
+    </footer>
+  )
+}
+
+export default Rodape
